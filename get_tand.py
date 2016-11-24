9bb76e0b132881d0f815248cdc64d1f6caf1a45b
mahuanhuan <824352882@qq.com>
2016-11-24 14:08:21 +0800

    complete getting jour-url

--- get_tand.py
+++ get_tand.py
@@ -3,17 +3,18 @@ import get_url
 
 class Tand(get_url.Site):
 
-    def __init__(self, site)
+    def __init__(self, site):
         super(Tand, self).__init__(site)
 
     def _journal_url_g(self):
-        for data1 in home_list:
+        print len(self.home_list)
+        for data1 in self.home_list:
             index_url = data1[0]
             for home_url, name in data1[1]:
                 if home_url.endswith('/current'):
                     home_url = home_url[:-len('/current')].replace('/toc/', '/loi/')
                 yield home_url, name
 
-if __name__ == __main__:
+if __name__ == '__main__':
     t = Tand('tandfonline')
     t._get_journal_url()
--- get_url.py
+++ get_url.py
@@ -3,13 +3,14 @@ import pickle
 
 class Site(object):
     def __init__(self, site ):
-        this.site = site
-        this.path = '/home/disk1/ps/se/scholar-journal/en/luyao/%s/article' % site
+        self.site = site
+        self.path = '/home/disk1/ps/se/scholar-journal/en/luyao/%s/home' % site
 
     def _get_journal_url(self):
-        obj = pickle.load(path)
-        home_list = obj.home_list
-        f = open(site + '/journal_url', 'w')
+        obj = pickle.load(open(self.path))
+        self.home_list = obj.home_list
+        print 'father home_list len: %d' % len(self.home_list) 
+        f = open(self.site + '/journal_url', 'w')
         try:
             for url, name in self._journal_url_g():
                 f.write(name + '\t' + url + '\n')
